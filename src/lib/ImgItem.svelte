1614eeb8287bb6fab27ab40c29c8c6651ad63611
uriberto <uri.andrews@gmail.com>
2021-11-23 03:31:16 -0600

    removed rel external

--- src/lib/ImgItem.svelte
+++ src/lib/ImgItem.svelte
@@ -8,6 +8,6 @@
 	}
 </script>
 
-<a rel="external" href="{link}" class="xlg:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 px-2 mb-4">
+<a href="{link}" class="xlg:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 px-2 mb-4">
 	<img style="width:100%;height:auto;" class="rounded-3xl " class:cursor-pointer={link} src="{src}"/>
 </a>
